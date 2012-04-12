8302d7aed8dcf3c271f4675b0cd11c2cf9b9b9e2
Martin Večeřa <marvenec@gmail.com>
2012-04-12 22:13:13 +0200

    userstatus enum

---
+++ src/main/java/org/jboss/picketlink/idm/auth/UserStatus.java
@@ -0,0 +1,10 @@
+package org.jboss.picketlink.idm.auth;
+
+/**
+ * 
+ * @author <a href="mailto:mvecera@redhat.com">Martin Večeřa</a>
+ *
+ */
+public enum UserStatus {
+	OK, DISABLED, UNEXISTING, NOTASSIGNEDTOROLE, WRONGPASSWORD
+}
